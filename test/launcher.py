129ce4282d5068757cc3b02b3527ed8074626687
Martin Jaros <xjaros32@stud.feec.vutbr.cz>
2016-03-30 22:14:08 +0200

    fix typo in pkg-config, test launch installed binary

--- test/launcher.py
+++ test/launcher.py
@@ -16,13 +16,12 @@ if GRAPHS:
     os.environ.update({'GST_DEBUG_DUMP_DOT_DIR': '.'})
     subprocess.call('rm -f *-pipeline.dot.svg', shell=True)
 
-execpath = '../src/nanotalk'
 args1 = 'nanotalk -s incoming-call.ogg -k test1.key -a aliases.txt'
 args2 = 'nanotalk -s incoming-call.ogg -k test2.key -a aliases.txt -l 5005 -h localhost'
 
-proc1 = subprocess.Popen(args1.split(), executable=execpath, env=os.environ)
+proc1 = subprocess.Popen(args1.split(), env=os.environ)
 time.sleep(.1)
-proc2 = subprocess.Popen(args2.split(), executable=execpath, env=os.environ)
+proc2 = subprocess.Popen(args2.split(), env=os.environ)
 
 try:
     proc1.wait()
